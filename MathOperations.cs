5dcd9559004e8e12b1c0f1494035448d2929c16d
Qualified Rocket <qualifiedrocket@gmail.com>
2021-02-11 14:34:07 -0500

    Adding pre-existing files to repository

---
+++ MathOperations.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace EquationSolverProject
+{
+    public static class MathOperations
+    {
+
+        // Basic Math
+
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public static int Mult(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+        public static int Div(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+        public static int Rem(int num1, int num2)
+        {
+            return num1 % num2;
+        }
+
+
+        // Solve for _
+
+        public static int SolveForY(int a, int x, int b)
+        {
+            int y = (a * x) + b;
+            return y;
+        }
+
+    }
+}
